d095c6bbfb4221961308b2e1e6d7856f21cfd995
Chinmay C Bhat <139462368+chinmayc0707@users.noreply.github.com>
2026-03-27 14:48:58 +0000

    bug fic

--- weekend.py
+++ weekend.py
@@ -64,7 +64,7 @@ def index():
             'dining': request.form['dining'],
             'accommodation': request.form['accommodation'],
             'conditions': request.form['conditions'],
-            'additional_details': request.form.get('additional_details', '')
+            'additional_details': request.form.get('additional_details', ''),
             'conditions': request.form['conditions']
         }
 
